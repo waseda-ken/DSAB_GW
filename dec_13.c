2a278c871332b7cf97a9eacf4d0ec2c36231ee82
Nobuhiro Oto <nobuhirooto@gmail.com>
2024-12-26 06:27:13 +0900

    dec

--- dec_13.c
+++ dec_13.c
@@ -4,7 +4,7 @@
 
 int dec(){
   FILE *sfp;
-  if((sfp = fopen(ENCDATA, "r")) ==NULL){
+  if((sfp = fopen(SEQDATA, "r")) ==NULL){
     fprintf(stderr, "cannot open %s\n", SEQDATA);
     exit(1);
   }
